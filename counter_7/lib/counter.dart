f89be53ad261abcef08b808293b738a5a63a30ed
8adamw <104297172+8adamw@users.noreply.github.com>
2022-12-28 10:34:14 +0700

    revisi tugas 8

--- counter_7/lib/counter.dart
+++ counter_7/lib/counter.dart
@@ -1,59 +1,14 @@
-import 'package:counter_7/tambahbudget.dart';
-import 'package:counter_7/counter.dart';
-import 'package:counter_7/databudget.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-@override
-  Widget build(BuildContext context) {
-    return Provider(
-      create: (BuildContext context) {  },
-      child: MaterialApp(
-        title: 'PBP Flutter',
-        routes: {
-            "/tambah": (BuildContext context) => const SubmitPage(),
-        },
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+  int _counter = 0;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  State<CounterPage> createState() => _CounterPage();
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -120,4 +75,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  
+  void setState(Null Function() param0) {}
 }
---
+++ counter_7/lib/drawer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DrawerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(children: [
+        ListTile(
+            title: const Text('Counter'),
+            onTap: () {
+              Navigator.pushNamed(context, '/counter');
+            }),
+        ListTile(
+            title: const Text('Tambah Budget'),
+            onTap: () async {
+                Navigator.pushNamed(context, '/tambah');  
+            },
+        ),
+        ListTile(
+            title: const Text('Data Budget'),
+            onTap: () async {
+                Navigator.pushNamed(context, '/data');
+            },
+        ),
+      ]),
+    );
+  }
+}
